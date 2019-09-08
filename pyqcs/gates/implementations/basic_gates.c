12e423b56e59b8685c9d5bd785353c5e21cee648
Daniel Knüttel <daniel.knuettel@daknuett.eu>
2019-09-08 20:34:53 +0200

    added R, H; the latter does not work yet

--- pyqcs/gates/implementations/basic_gates.c
+++ pyqcs/gates/implementations/basic_gates.c
@@ -3,6 +3,7 @@
 #include <numpy/ndarraytypes.h>
 #include <numpy/ufuncobject.h>
 #include <stddef.h>
+#include <math.h>
 
 #include "generic_setup.h"
 
@@ -16,8 +17,27 @@ typedef struct
 long int ipow(int base
             , int exponent)
 {
-
     long int result;
+
+    // Actually most of our powers will be
+    // base 2, so handle this explicitly.
+    // Should be way faster.
+    if(base == 2)
+    {
+        result = 1;
+        result <<= exponent;
+        return result;
+    }
+    // Sometimes we have base -1 powers;
+    // these too, are easy to compute.
+    if(base == -1)
+    {
+        if(exponent & 0b1)
+        {
+            return -1;
+        }
+        return 1;
+    }
     if(exponent == 0)
     {
         return 1;
@@ -46,8 +66,72 @@ ufunc_X( char ** args
 
     for(i = 0; i < ndim; i++)
     {
-        qm_out[i] = qm_in[i^ipow(2, l)];
+        qm_out[i] = qm_in[i ^ ipow(2, l)];
     }
+    *measured_out = 0;
+}
+
+static void
+ufunc_R( char ** args
+       , npy_intp * dimensions
+       , npy_intp * steps
+       , void * data)
+{
+
+    basic_gate_argument_t argument = *((basic_gate_argument_t *) data);
+    npy_intp l = argument.act;
+    npy_intp i;
+    PYQCS_GATE_GENERIC_SETUP;
+
+    for(i = 0; i < ndim; i++)
+    {
+        if(i & argument.act)
+        {
+            qm_out[i].real = cos(argument.r) * qm_in[i].real;
+            qm_out[i].imag = sin(argument.r) * qm_in[i].imag;
+        }
+        else
+        {
+            qm_out[i] = qm_in[i];
+        }
+    }
+    *measured_out = 0;
+}
+
+static void
+ufunc_H( char ** args
+       , npy_intp * dimensions
+       , npy_intp * steps
+       , void * data)
+{
+    basic_gate_argument_t argument = *((basic_gate_argument_t *) data);
+    PYQCS_GATE_GENERIC_SETUP;
+    npy_intp i;
+
+    for(i = 0; i < ndim; i++)
+    {
+        npy_intp not_index = i ^ ipow(2, argument.act);
+
+        npy_intp sign;
+        if(!(i & (2 << argument.act))
+            && ipow(-1, not_index) == -1)
+        {
+            sign = -1;
+        }
+        else
+        {
+            sign = 1;
+        }
+
+        
+        qm_out[i].real = (qm_in[i].real 
+                             + sign * qm_in[not_index].real) 
+                         * M_SQRT1_2;
+        qm_out[i].imag = (qm_in[i].imag 
+                             + sign * qm_in[not_index].imag) 
+                         * M_SQRT1_2;
+    }
+
     *measured_out = 0;
 }
 
@@ -55,6 +139,10 @@ static char ufunc_types[5] =
     { NPY_CDOUBLE, NPY_DOUBLE, NPY_CDOUBLE, NPY_DOUBLE, NPY_DOUBLE };
 static PyUFuncGenericFunction ufunc_X_funcs[1] = 
     { ufunc_X };
+static PyUFuncGenericFunction ufunc_H_funcs[1] = 
+    { ufunc_H };
+static PyUFuncGenericFunction ufunc_R_funcs[1] = 
+    { ufunc_R };
 
 
 typedef struct
@@ -99,6 +187,52 @@ BasicGate_init
 				, PyUFunc_None // identity
 				, "X_function" // name
 				, "Computes the X (NOT) gate on a state." // doc
+				, 0 // unused
+                , "(n),(m)->(n),(m),()"); 
+
+            if(self->ufunc <= 0)
+            {
+                //I have no idea what is going on.
+                //PyErr_SetString(PyExc_ValueError, "failed to construct the ufunc for unknow reasons");
+                return -1;
+            }
+			break;
+		}
+		case 'H':
+		{
+			self->ufunc = PyUFunc_FromFuncAndDataAndSignature(
+				ufunc_H_funcs // func
+				, self->data // data
+				, ufunc_types //types
+				, 1 // ntypes
+				, 2 // nin
+				, 3 // nout
+				, PyUFunc_None // identity
+				, "H_function" // name
+				, "Computes the H (Hadamard) gate on a state." // doc
+				, 0 // unused
+                , "(n),(m)->(n),(m),()"); 
+
+            if(self->ufunc <= 0)
+            {
+                //I have no idea what is going on.
+                //PyErr_SetString(PyExc_ValueError, "failed to construct the ufunc for unknow reasons");
+                return -1;
+            }
+			break;
+		}
+		case 'R':
+		{
+			self->ufunc = PyUFunc_FromFuncAndDataAndSignature(
+				ufunc_R_funcs // func
+				, self->data // data
+				, ufunc_types //types
+				, 1 // ntypes
+				, 2 // nin
+				, 3 // nout
+				, PyUFunc_None // identity
+				, "R_function" // name
+				, "Computes the R (rotation) gate on a state." // doc
 				, 0 // unused
                 , "(n),(m)->(n),(m),()"); 
 
