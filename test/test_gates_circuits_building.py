d11d687cd207d5c23024798856d49024dabfa78d
Daniel Knüttel <daniel.knuettel@daknuett.eu>
2019-09-10 19:23:55 +0200

    some more tests

--- test/test_gates_circuits_building.py
+++ test/test_gates_circuits_building.py
@@ -1,7 +1,8 @@
 import pytest
 
-from pyqcs.gates.builtins import X
+from pyqcs.gates.builtins import X, H
 from pyqcs.gates.circuits import AnonymousCompoundGateCircuit
+from pyqcs.state.state import BasicState
 
 def test_or_creates_AnonymousCompoundGateCircuit():
     gc1 = X(0)
@@ -19,3 +20,11 @@ def test_or_preserves_order():
 
     assert circuit.to_gate_list() == (gc1.to_gate_list() + gc2.to_gate_list())
     assert gc1.to_gate_list() != gc2.to_gate_list()
+
+def test_XH():
+    M_SQRT1_2 = 0.70710678118654752440
+    state = BasicState.new_zero_state(2)
+
+    state = (X(0) | H(0)) * state
+
+    assert state._qm_state == pytest.approx([M_SQRT1_2, -M_SQRT1_2, 0, 0])
