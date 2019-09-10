7bb0d0321910e99cdabba6962d9b9a47d7d9f09b
Daniel Knüttel <daniel.knuettel@daknuett.eu>
2019-09-10 20:34:59 +0200

    removed some prints I forgot

--- src/pyqcs/gates/executor.py
+++ src/pyqcs/gates/executor.py
@@ -4,9 +4,6 @@ class GateListExecutor(object):
 
     def __call__(self, state):
         for gate in self._gate_list:
-            print(state._qm_state)
-            print(gate)
             state = state.apply_gate(gate)
-        print(state._qm_state)
         return state
 
