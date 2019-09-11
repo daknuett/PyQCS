82e5cba5ddc787c755d26a22aad1fed2a24209d5
Daniel Knüttel <daniel.knuettel@daknuett.eu>
2019-09-11 11:53:33 +0200

    added a test for 7afa23a

--- test/test_cicruit_construction.py
+++ test/test_cicruit_construction.py
@@ -13,3 +13,9 @@ def test_repeating_gates():
 
     assert multiplied.to_executor().to_gate_list() == (5 * base.to_executor().to_gate_list())
 
+def test_repeating_gates_inverted():
+    base = X(0) | H(0)
+
+    multiplied = 5 * base
+
+    assert multiplied.to_executor().to_gate_list() == (5 * base.to_executor().to_gate_list())
