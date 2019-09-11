902b93ab0e4fa63bdbef33e93d0518d9fd1c7b5f
Daniel Knüttel <daniel.knuettel@daknuett.eu>
2019-09-11 11:51:03 +0200

    added a test for multiplied gate circuits

---
+++ test/test_cicruit_construction.py
@@ -0,0 +1,15 @@
+import pytest
+
+from pyqcs import State, C, H, X, R
+
+@pytest.fixture
+def zero_state():
+    return State.new_zero_state(2)
+
+def test_repeating_gates():
+    base = X(0) | H(0)
+
+    multiplied = base * 5
+
+    assert multiplied.to_executor().to_gate_list() == (5 * base.to_executor().to_gate_list())
+
