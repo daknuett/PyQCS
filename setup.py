a58f527e0ba2dcc680f21d03b9384295ea290109
Daniel Knüttel <daniel.knuettel@daknuett.eu>
2019-11-11 13:36:59 +0100

    moved to v0.0.15

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ generic_gate = Extension(
 
 setup(
         name="pyqcs"
-        , version="0.0.14"
+        , version="0.0.15"
         , description="A quantum computing simulator."
         , long_description = open("README.rst").read()
         , ext_modules=[basic_gates
