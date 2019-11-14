71539d33dabcce5160071956bf5befbcceaaf19e
Daniel Knüttel <daniel.knuettel@daknuett.eu>
2019-11-14 14:09:44 +0100

    fixed a bug in BasicState.__eq__

--- src/pyqcs/state/state.py
+++ src/pyqcs/state/state.py
@@ -72,7 +72,8 @@ class BasicState(AbstractState):
             nonzeros = self._qm_state != 0
             phases = self._qm_state[nonzeros] / other._qm_state[nonzeros]
             angles = np.angle(phases)
-            phaseless = np.exp(-1j * np.average(angles)) * phases
+            print(angles)
+            phaseless = np.exp(-1j * np.max(angles)) * phases
             return np.allclose(phaseless, np.ones(len([n for n in nonzeros if n])))
 
         raise TypeError()
