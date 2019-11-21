b89e56fd56fb0447c11030f6bb6995cd0d27e4eb
Daniel Knüttel <daniel.knuettel@daknuett.eu>
2019-11-21 20:19:59 +0100

    added tests for sampling

--- test/test_random_circuits_against_sqc.py
+++ test/test_random_circuits_against_sqc.py
@@ -4,7 +4,7 @@ import pytest
 
 import sqc
 from pyqcs.util.random_circuits import random_basis_values
-from pyqcs import X, H, R, C, list_to_circuit, State
+from pyqcs import X, H, R, C, list_to_circuit, State, sample
 
 def get_circuits(random_basis_values, nqbits):
 
@@ -72,3 +72,45 @@ def test_random_q10_l100():
     for _ in range(4000):
         do_test_q10_l100()
 
+
+def do_test_q4_l10_sample_half():
+    qcs = State.new_zero_state(4)
+    sq = sqc.state(4)
+    random_values = random_basis_values(4, 10)
+    qcs_circuit, sqc_circuit = get_circuits(random_values, 4)
+
+    sq = sqc_circuit * sq
+    qcs = qcs_circuit * qcs
+
+    np.random.seed(0xdeadbeef)
+    result_sqc = sqc.sample(sq, 10, [0, 1])
+    np.random.seed(0xdeadbeef)
+    result_pyqcs = sample(qcs, 0b11, 10)
+
+    assert result_sqc == result_pyqcs
+
+@pytest.mark.slow
+def test_random_q10_l100_sample_half():
+    for i in range(1000):
+        do_test_q10_l100_sample_half()
+
+def do_test_q10_l100_sample_half():
+    qcs = State.new_zero_state(10)
+    sq = sqc.state(10)
+    random_values = random_basis_values(10, 100)
+    qcs_circuit, sqc_circuit = get_circuits(random_values, 10)
+
+    sq = sqc_circuit * sq
+    qcs = qcs_circuit * qcs
+
+    np.random.seed(0xdeadbeef)
+    result_sqc = sqc.sample(sq, 10, list(range(5)))
+    np.random.seed(0xdeadbeef)
+    result_pyqcs = sample(qcs, 0b11111, 10)
+
+    assert result_sqc == result_pyqcs
+
+@pytest.mark.slow
+def test_random_q4_l10_sample_half():
+    for i in range(100):
+        do_test_q4_l10_sample_half()
