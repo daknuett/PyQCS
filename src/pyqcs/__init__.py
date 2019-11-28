9e0d7f34fd826d340c33384119acac2ec9090806
Daniel Knüttel <daniel.knuettel@daknuett.eu>
2019-11-28 10:18:19 +0100

    rewrote measurement and sampling to improve readability

--- src/pyqcs/__init__.py
+++ src/pyqcs/__init__.py
@@ -31,16 +31,23 @@ from .gates.builtins import C, H, X, R, M, Z, CX, CZ, GenericGate
 from .state.state import BasicState as State
 from .gates.circuits import AnonymousCompoundGateCircuit, NamedCompoundGateCircuit
 
-def measure(state, bit_mask):
+def list_to_circuit(list_of_circuits, name=None):
+    if(not name):
+        return AnonymousCompoundGateCircuit(list_of_circuits)
+    return NamedCompoundGateCircuit(list_of_circuits, name)
+
+def build_measurement_circuit(bit_mask):
     if(isinstance(bit_mask, int)):
         c_list = [M(i) for i in range(bit_mask.bit_length()) if bit_mask & (1 << i)]
     elif(isinstance(bit_mask, (list, tuple))):
         c_list = [M(i) for i in bit_mask]
     else:
         raise TypeError("bit_mask must be either int, list or tuple, but {} is given".format(type(bit_mask)))
-    circuit = c_list[0]
-    for c in c_list[1:]:
-        circuit |= c
+    return list_to_circuit(c_list)
+
+
+def measure(state, bit_mask):
+    circuit = build_measurement_circuit(bit_mask)
 
     state = State(state._qm_state, np.array([-1 for i in state._cl_state], dtype=np.int8), state._nbits, 0)
     new_state = circuit * state
@@ -53,15 +60,7 @@ def _do_sample(state, circuit, nsamples):
         yield new_state, sum([1 << i for i,v in enumerate(new_state._cl_state) if v == 1])
 
 def sample(state, bit_mask, nsamples, keep_states=False):
-    if(isinstance(bit_mask, int)):
-        c_list = [M(i) for i in range(bit_mask.bit_length()) if bit_mask & (1 << i)]
-    elif(isinstance(bit_mask, (list, tuple))):
-        c_list = [M(i) for i in bit_mask]
-    else:
-        raise TypeError("bit_mask must be either int, list or tuple, but {} is given".format(type(bit_mask)))
-    circuit = c_list[0]
-    for c in c_list[1:]:
-        circuit |= c
+    circuit = build_measurement_circuit(bit_mask)
 
     state = State(state._qm_state, np.array([-1 for i in state._cl_state], dtype=np.int8), state._nbits, 0)
 
@@ -69,8 +68,3 @@ def sample(state, bit_mask, nsamples, keep_states=False):
         return Counter(_do_sample(state, circuit, nsamples))
 
     return Counter((i[1] for i in _do_sample(state, circuit, nsamples)))
-
-def list_to_circuit(list_of_circuits, name=None):
-    if(not name):
-        return AnonymousCompoundGateCircuit(list_of_circuits)
-    return NamedCompoundGateCircuit(list_of_circuits, name)
