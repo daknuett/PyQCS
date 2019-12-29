3a52a4b3b864aadc282a3aaaa4f9c45599b052c7
Daniel Knüttel <daniel.knuettel@daknuett.eu>
2019-12-29 19:00:39 +0100

    fixed a bug in the random circuit generator

--- src/pyqcs/util/random_circuits.py
+++ src/pyqcs/util/random_circuits.py
@@ -12,16 +12,6 @@ def random_basis_values(nqbits, ngates):
 
     return (gate_index, entanglement_index, phi)
 
-def random_circuit(nqbits, ngates, M1, M2, M3, M4):
-    """
-    Sample ngates gates from (M1, M2, M3, M4) where Mi act on one of the nqbit
-    qbits, M1, M2 take no extra arguments (typically M1=X, M2=H), M3 takes a
-    float argument (typically M3=R_phi) and M4 is an entanglement gate (typically CX or CZ).
-
-    M1, M2, M3, M4 are equally likely, all qbits are equally likely to be modified.
-    Both x for M3 and k for M4 are uniformely distributed.
-    """
-    return list_to_circuit(list(_random_gates(nqbits, ngates, M1, M2, M3, M4)))
 def _random_gates(nqbits, ngates, M1, M2, M3, M4):
     for i, k, x in zip(*random_basis_values(nqbits, ngates)):
         if(i <= nqbits):
@@ -32,9 +22,20 @@ def _random_gates(nqbits, ngates, M1, M2, M3, M4):
             yield M3(i - 2*nqbits - 1, x)
 
         else:
-            if(k <= i):
+            if(k <= i - 3*nqbits - 1):
                 k -= 1
             yield M4(i - 3*nqbits - 1, k)
+
+def random_circuit(nqbits, ngates, M1, M2, M3, M4):
+    """
+    Sample ngates gates from (M1, M2, M3, M4) where Mi act on one of the nqbit
+    qbits, M1, M2 take no extra arguments (typically M1=X, M2=H), M3 takes a
+    float argument (typically M3=R_phi) and M4 is an entanglement gate (typically CX or CZ).
+
+    M1, M2, M3, M4 are equally likely, all qbits are equally likely to be modified.
+    Both x for M3 and k for M4 are uniformely distributed.
+    """
+    return list_to_circuit(list(_random_gates(nqbits, ngates, M1, M2, M3, M4)))
 
 def random_circuit_XHRC(nqbits, ngates):
     return random_circuit(nqbits, ngates, X, H, R, C)
