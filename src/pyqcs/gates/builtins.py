935fb5641d52264541a9901ddbae2bff5f1cc585
Daniel Knüttel <daniel.knuettel@daknuett.eu>
2020-01-08 08:07:50 +0100

    added that controlled gates also require the control qbit

--- src/pyqcs/gates/builtins.py
+++ src/pyqcs/gates/builtins.py
@@ -6,16 +6,22 @@ from ..graph.gate import GraphGate, CLOperation, CZOperation, MeasurementOperati
 
 class BuiltinGateBuilder(AbstractSingleGateCircuitBuilder):
     __slots__ = ["_type"]
-    def __init__(self, type_, get_graph_gate):
+    def __init__(self, type_, get_graph_gate, has_control=False):
         AbstractSingleGateCircuitBuilder.__init__(self)
         self._type = type_
         self.get_graph_gate = get_graph_gate
+        self._has_control = has_control
 
     def __call__(self, act, *args):
         if((act, *args) not in self._registry):
             gate = BuiltinGate(self._type, act,*args)
             gate_graph = self.get_graph_gate(act, *args)
-            circuit = SingleGateCircuit((1 << act)
+
+            uses_qbits = (1 << act)
+            if(self._has_control):
+                uses_qbits |= (1 << args[0])
+
+            circuit = SingleGateCircuit(uses_qbits
                         , []
                         , self._type + "(" + ",".join((str(a) for a in args)) + ")"
                         , (self._type, act, *args)
@@ -53,7 +59,7 @@ def _get_graph_C_gate(act, control, i2):
     return GraphGate([CLOperation(act, 0)
                     , CZOperation(act, control)
                     , CLOperation(act, 0)])
-_C = BuiltinGateBuilder('C', _get_graph_C_gate)
+_C = BuiltinGateBuilder('C', _get_graph_C_gate, has_control=True)
 def C(act, control):
     if(act < 0):
         raise ValueError("act qbit must be >= 0")
@@ -80,7 +86,7 @@ def Z(act):
 
 def _get_graph_B_gate(act, control, i2):
     return GraphGate([CZOperation(act, control)])
-_B = BuiltinGateBuilder('B', _get_graph_B_gate)
+_B = BuiltinGateBuilder('B', _get_graph_B_gate, has_control=True)
 def CZ(act, control):
     if(act < 0):
         raise ValueError("act qbit must be >= 0")
