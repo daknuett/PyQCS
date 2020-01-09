938a64b18d052bb6f0893982cb2a0f7fa6ff14bd
Daniel Knüttel <daniel.knuettel@daknuett.eu>
2020-01-09 08:58:35 +0100

    added randomized tests for converting graphs to circuits

--- test/test_graph_to_circuit.py
+++ test/test_graph_to_circuit.py
@@ -1,6 +1,11 @@
+from numpy import random
+import pytest
+
 from pyqcs import X, H, S, CX, CZ, list_to_circuit, State
 from pyqcs.graph.state import GraphState
 from pyqcs.util.to_circuit import graph_state_to_circuit
+from pyqcs.util.random_circuits import random_circuit
+from pyqcs.util.to_diagram import circuit_to_diagram
 
 def test_EPR_state():
     g = GraphState.new_zero_state(20)
@@ -11,3 +16,31 @@ def test_EPR_state():
     applied = graph_state_to_circuit(g) * State.new_zero_state(20)
 
     assert applied == g.to_naive_state()
+
+def S_with_extra_arg(act, i):
+    return S(act)
+
+def do_test_q4_l10():
+    naive = State.new_zero_state(4)
+    graph = GraphState.new_zero_state(4)
+    circuit = random_circuit(4, 10, X, H, S_with_extra_arg, CZ)
+    print("original")
+    print(circuit_to_diagram(circuit))
+
+    naive = circuit * naive
+    graph = circuit * graph
+    ng = graph_state_to_circuit(graph) * State.new_zero_state(4)
+    print("converted")
+    print(circuit_to_diagram(graph_state_to_circuit(graph)))
+
+    if(naive != ng):
+        print("naive", naive)
+        print("graph", graph.to_naive_state())
+        print("from circuit", ng)
+    assert naive == ng
+
+@pytest.mark.slow
+def test_random_q4_l10():
+    random.seed(2)
+    for _ in range(5000):
+        do_test_q4_l10()
