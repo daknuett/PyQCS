9d0b3b2e34c96ad9a0efcc20429b2c9d21d507f5
Daniel Knüttel <daniel.knuettel@daknuett.eu>
2020-01-15 14:29:38 +0100

    added test using randomly generated graph states

---
+++ test/test_graph_CZ_on_random_graph5.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+from pyqcs.util.random_graphs import random_graph_lists
+from pyqcs.util.from_lists import graph_lists_to_graph_state
+from pyqcs import CZ
+
+def do_test5_5():
+    lists = random_graph_lists(5, 5)
+    g = graph_lists_to_graph_state(lists)
+    n =  CZ(0, 1) * g.to_naive_state()
+    g = CZ(0, 1) * g
+
+    if(g.to_naive_state() != n):
+        print("before:", lists)
+        print("got:", g._g_state.to_lists())
+
+        print()
+
+        print("expected")
+        print(n)
+        print("got")
+        print(g.to_naive_state())
+
+    assert g.to_naive_state() == n
+
+def test_random_graph5_5():
+    np.random.seed(1)
+    for _ in range(1000):
+        do_test5_5()
