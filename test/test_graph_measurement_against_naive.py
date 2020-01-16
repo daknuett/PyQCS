bfdf047699ce5400702125ad6c5efebd0eca6fe8
Daniel Knüttel <daniel.knuettel@daknuett.eu>
2020-01-16 10:51:54 +0100

    added another randomized test for measurements

--- test/test_graph_measurement_against_naive.py
+++ test/test_graph_measurement_against_naive.py
@@ -55,9 +55,61 @@ def do_test_q4_l10():
             print({k: v.to_naive_state() for k,v in results_graph.items()})
         assert vn == vg.to_naive_state()
 
-#@pytest.mark.slow
+@pytest.mark.slow
 def test_random_q4_l10_m0():
     np.random.seed(0xdeadbeef)
     for _ in range(1000):
         do_test_q4_l10()
 
+def do_test_q5_l100():
+    naive = State.new_zero_state(5)
+    graph = GraphState.new_zero_state(5, force_new_state=True)
+    circuit = random_circuit(5, 100, X, H, S_with_extra_arg, CZ)
+
+    naive = circuit * naive
+    graph = circuit * graph
+
+    results_naive = {}
+    results_graph = {}
+    cnt = 0
+    while((0 not in results_naive
+                or 1 not in results_naive
+                or 0 not in results_graph
+                or 1 not in results_graph)
+            and cnt < 100):
+        g_bar = M(0) * graph
+        n_bar = M(0) * naive
+
+        if(n_bar._cl_state[0] in results_naive
+                and g_bar._measured[0] in results_graph):
+            cnt += 1
+        results_naive[n_bar._cl_state[0]] = n_bar
+        results_graph[g_bar._measured[0]] = g_bar
+
+    for (kn, vn), (kg, vg) in zip(sorted(results_naive.items())
+                                , sorted(results_graph.items())):
+        assert kn == kg
+        vn._cl_state[0] = -1
+        if(vn != vg.to_naive_state()):
+            print("before:")
+            print(naive)
+            print("lists:", graph._g_state.to_lists())
+            print()
+            print("expected:")
+            print(vn)
+            print("got:")
+            print(vg.to_naive_state())
+            print("lists:", vg._g_state.to_lists())
+            print()
+            print("all results:")
+            print("expect:")
+            print(results_naive)
+            print("got:")
+            print({k: v.to_naive_state() for k,v in results_graph.items()})
+        assert vn == vg.to_naive_state()
+
+@pytest.mark.slow
+def test_random_q5_l100_m0():
+    np.random.seed(0xdeadbeef)
+    for _ in range(1000):
+        do_test_q5_l100()
