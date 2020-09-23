9fc47316f9a5ec79eaeba0968d2e524e4820c2ae
Daniel Knüttel <daniel.knuettel@daknuett.eu>
2020-09-23 12:19:22 +0200

    improved information given in setup.py for distributions

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ generic_gate = Extension(
 
 setup(
         name="pyqcs"
-        , version="2.2.3"
+        , version="2.2.4"
         , description="A quantum computing simulator."
         , long_description = open("README.rst").read()
         , ext_modules=[basic_gates
@@ -49,5 +49,6 @@ setup(
         , classifiers=[
             "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
         ]
+        , license="GNU General Public License v3 (GPLv3)"
     )
 
