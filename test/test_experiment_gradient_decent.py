3fe65b88b0b484fc245c8ddaa8c7009ee9afa7b6
Daniel Knüttel <daniel.knuettel@daknuett.eu>
2020-10-22 15:28:52 +0200

    marked a test as slow

--- test/test_experiment_gradient_decent.py
+++ test/test_experiment_gradient_decent.py
@@ -5,6 +5,7 @@ import numpy as np
 from pyqcs.experiment.gradient_decent import approx_gradient_decent
 from pyqcs.experiment.workflow import WorkflowSpawner, FunctionInstruction
 
+@pytest.mark.slow
 def test_gradient_decent_convergence(ray_setup):
     f = lambda x: np.sum(x**2 + x**6)
     x0 = np.array(list(range(3)))
