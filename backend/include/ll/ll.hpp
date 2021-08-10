fb92f47a1547b963fcb7c7117e645fce459ce056
Daniel Knüttel <daniel.knuettel@daknuett.eu>
2021-08-10 17:15:13 +0200

    added a linked list implementation

---
+++ backend/include/ll/ll.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include <iostream>
+
+namespace ll
+{
+    class Node
+    {
+        private:
+        Node * m_next;
+        int m_value;
+        public:
+        Node(int value);
+        Node(int value, Node * next);
+        void print(void);
+        friend class SortedList;
+
+    };
+    class SortedList
+    {
+        private:
+        Node * m_first;
+        public:
+        void insert(int value);
+        bool has_value(int value);
+        void print(void);
+        void delete_value(int value);
+        SortedList(void);
+        ~SortedList(void);
+
+    };
+}
---
+++ backend/src/ll/ll.cpp
@@ -0,0 +1,123 @@
+#include <ll/ll.hpp>
+namespace ll
+{
+    Node::Node(int value): m_value(value)
+    {
+        m_next = NULL;
+    }
+    Node::Node(int value, Node * next): m_next(next), m_value(value){}
+    void Node::print(void)
+    {
+        if(m_next != NULL)
+        {
+            std::cout << m_value << " -> ";
+        }
+        else
+        {
+            std::cout << m_value << " ";
+        }
+    }
+
+    void SortedList::insert(int value)
+    {
+        Node * c_node = m_first;
+        Node * p_node = NULL;
+        while(c_node != NULL)
+        {
+            if(c_node->m_value < value)
+            {
+                p_node = c_node;
+                c_node = c_node->m_next;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        if(p_node == NULL)
+        {
+            m_first = new Node(value, c_node);
+        }
+        else
+        {
+            p_node->m_next = new Node(value, c_node);
+        }
+    }
+
+    bool SortedList::has_value(int value)
+    {
+        Node * c_node = m_first;
+        while(c_node != NULL)
+        {
+            if(c_node->m_value < value)
+            {
+                c_node = c_node->m_next;
+                continue;
+            }
+            if(c_node->m_value > value)
+            {
+                break;
+            }
+            if(c_node->m_value == value)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void SortedList::print(void)
+    {
+        Node * c_node = m_first;
+        std::cout << "[- ";
+        while(c_node != NULL)
+        {
+            c_node->print();
+            c_node = c_node->m_next;
+        }
+        std::cout << "@\n";
+    }
+    void SortedList::delete_value(int value)
+    {
+        Node * c_node = m_first;
+        Node * p_node;
+        while(c_node != NULL)
+        {
+            if(c_node->m_value < value)
+            {
+                p_node = c_node;
+                c_node = c_node->m_next;
+                continue;
+            }
+            if(c_node->m_value == value)
+            {
+                if(p_node == NULL)
+                {
+                    m_first = c_node->m_next;
+                }
+                else
+                {
+                    p_node->m_next = c_node->m_next;
+                }
+                delete c_node;
+                break;
+            }
+            break;
+        }
+    }
+    SortedList::SortedList(void)
+    {
+        m_first = NULL;
+    }
+    SortedList::~SortedList(void)
+    {
+        Node * c_node = m_first;
+        while(c_node != NULL)
+        {
+            Node * t_node = c_node->m_next;
+            delete c_node;
+            c_node = t_node;
+        }
+    }
+}
