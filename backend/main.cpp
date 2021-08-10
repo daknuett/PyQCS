497b784a2931b4d8e7f9489f3042e1820fe3667a
Daniel Knüttel <daniel.knuettel@daknuett.eu>
2021-08-10 21:39:45 +0200

    I am just using this file for live testing

--- backend/main.cpp
+++ backend/main.cpp
@@ -6,56 +6,19 @@
 int
 main(int argc, char ** argv)
 {
-    ll::SortedList list;
-
-    std::cout << "LL:\n";
-    list.print();
-
-    list.insert(10);
-    list.insert(1);
-    list.insert(3);
-    list.print();
-    list.insert(2);
-    list.insert(11);
-    list.insert(20);
-
-    list.print();
-
-    list.delete_value(3);
-
-    list.print();
-
-    std::cout << "has 11: " << list.has_value(11) << "\n";
-    std::cout << "has 3: " << list.has_value(3) << "\n";
-    std::cout << "has 21: " << list.has_value(21) << "\n";
-
-    std::cout << "RBT:\n";
 
     rbt::RBTree tree;
     std::vector<int> vect;
-    tree.insert(10);
-    tree.insert(1);
+
+    tree.insert(5);
+    tree.insert(4);
     tree.insert(3);
-
-    tree.export_inorder(vect);
-    std::cout << "tree contains:";
-    for(auto i: vect)
-    {
-        std::cout << ", " << i;
-    }
-    std::cout << "\n";
     tree.insert(2);
-    tree.insert(11);
-    tree.insert(20);
-    tree.export_inorder(vect);
-    std::cout << "tree contains:";
-    for(auto i: vect)
-    {
-        std::cout << ", " << i;
-    }
-    std::cout << "\n";
+    tree.insert(1);
+    tree.to_dot(std::cout);
+    
 
-
-
+    //tree.insert(11);
+    //tree.insert(20);
     return 0;
 }
