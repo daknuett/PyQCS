f6d7559dd47d35fb543559a66086b4b1e60c3728
Daniel Knüttel <daniel.knuettel@daknuett.eu>
2021-08-10 21:40:03 +0200

    added some proper tests.

---
+++ backend/tests/rbtree_insert.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+#include <random>
+#include <algorithm>
+#include <rbt/rbt.hpp>
+
+#ifndef NTEST
+#define NTEST 10000
+#endif
+
+int
+main(int argc, char ** argv)
+{
+    std::vector<int> orig;
+    for(auto i = 0; i < NTEST; i++)
+    {
+        orig.push_back(i);
+    }
+
+    std::vector<int> copy = orig;
+
+    std::mt19937 rne;
+    std::shuffle(copy.begin(), copy.end(), rne);
+
+    rbt::RBTree tree;
+    for(auto i: copy)
+    {
+        tree.insert(i);
+    }
+
+    std::vector<int> exported;
+    tree.export_inorder(exported);
+
+    if(exported.size() != orig.size())
+    {
+        return -1;
+    }
+    for(int i = 0; i < exported.size(); i++)
+    {
+        if(exported[i] != orig[i])
+        {
+            return -1;
+        }
+    }
+    return 0;
+}
