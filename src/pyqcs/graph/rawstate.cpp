c952ff4e9437aecf6efc0420cbfef38fa4ab9120
Daniel Knüttel <daniel.knuettel@daknuett.eu>
2021-09-19 12:32:56 +0200

    removed some debug messages

--- src/pyqcs/graph/rawstate.cpp
+++ src/pyqcs/graph/rawstate.cpp
@@ -103,11 +103,8 @@ RawGraphState_apply_CZ(RawGraphState * self, PyObject * args)
         return NULL;
     }
 
-    std::cerr << "applying CZ(" << i << ", " << j << ")" << std::endl;
-    std::cerr << "state is: " << self->state << std::endl;
 
     self->state->apply_CZ(i, j);
-    std::cerr << "application done" << std::endl;
 
     Py_RETURN_NONE;
 }
