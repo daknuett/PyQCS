3c0d7d4fa270ddb78bc11984edaa206bda2484f7
Daniel Knüttel <daniel.knuettel@daknuett.eu>
2021-09-24 10:34:07 +0200

    updated bytecode generation to latest circuit implementation

--- src/pyqcs/util/bytecode.py
+++ src/pyqcs/util/bytecode.py
@@ -1,7 +1,13 @@
 import struct
 from collections import deque
 
-from ..gates.builtins import CLOperation, CZOperation, MeasurementOperation
+_local_clifford_gates = ["H", "S", "X", "Z"]
+_CZ_gates = ["CZ"]
+_decomposable_gates = ["CX"]
+_measurement_gates = ["M"]
+
+_local_clifford_gate_vop = {"H": 0, "S": 1, "X": 14, "Z": 5}
+_gate_decompositions = {"CX": (("H", 0), ("CZ", 0, 1), ("H", 0))}
 
 
 class ByteCodeHeader(object):
@@ -38,6 +44,7 @@ class ByteCodeHeader(object):
 
         return magic_word + b + b"b" + s + b"s" + q + b"q" + b"".join(samples) + b"\xff"*8
 
+
 class ByteCodeInstruction(object):
     cmds = {"M": b"M", "CZ": b"Z", "C_L": b"L"}
 
@@ -49,6 +56,31 @@ class ByteCodeInstruction(object):
     def to_bytes(self):
         return self._command + struct.pack("Q", self._act) + struct.pack("Q", self._argument)
 
+
+def clifford2i(gate):
+    return [ByteCodeInstruction("C_L", gate._act, _local_clifford_gate_vop[gate._name])]
+
+
+def cz2i(gate):
+    return [ByteCodeInstruction("CZ", gate._act, gate._control)]
+
+
+def measure2i(gate):
+    return [ByteCodeInstruction("M", gate._act, 0)]
+
+
+def decomposable2i(gate):
+    args = (gate._act, gate._control)
+    result = []
+    for word in _gate_decompositions[gate._name]:
+        if(len(word) == 2):
+            decomp = ByteCodeInstruction("C_L", args[word[1]], _local_clifford_gate_vop[word[0]])
+        else:
+            decomp = ByteCodeInstruction(gate._name, args[word[1]], args[word[2]])
+        result.append(decomp)
+    return result
+
+
 def circuit_to_byte_code(header, circuit, add_header=True):
     if(not circuit._has_graph):
         raise TypeError("Circuit must be applicable to graph.")
@@ -58,21 +90,12 @@ def circuit_to_byte_code(header, circuit, add_header=True):
     executor = circuit._executor(circuit.get_child_executors(graph=True))
     gates = executor.to_gate_list()
 
-    def C_L_to_instruction(op):
-        return ByteCodeInstruction("C_L", op._act, op._clifford_index)
-    def CZ_to_instruction(op):
-        return ByteCodeInstruction("CZ", op._act, op._control)
-    def M_to_instruction(op):
-        return ByteCodeInstruction("M", op._act, 0)
-
-    o2i = {CLOperation: C_L_to_instruction
-            , CZOperation: CZ_to_instruction
-            , MeasurementOperation: M_to_instruction}
+    o2i = {"CZ": cz2i, "M": measure2i, "CX": decomposable2i}
+    o2i.update({gn: clifford2i for gn in _local_clifford_gates})
 
     instructions = deque()
     for gate in gates:
-        for op in gate._operation_list:
-            instructions.append(o2i[type(op)](op))
+        instructions.extend(o2i[gate._name](gate))
 
     if(add_header):
         result = header.to_bytes()
