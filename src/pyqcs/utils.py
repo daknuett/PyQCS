7946fc1a5f3c2753d3248792fe88b9d535fbfebb
Daniel Knüttel <daniel.knuettel@daknuett.eu>
2021-10-13 11:41:50 +0200

    fixed a gotcha where passing generators to list_to_circuit does not work

--- src/pyqcs/utils.py
+++ src/pyqcs/utils.py
@@ -5,6 +5,9 @@ from .util.to_diagram import CircuitPNGFormatter
 def list_to_circuit(list_of_circuits):
     if(not list_of_circuits):
         return
+    if(not isinstance(list_of_circuits, list)):
+        list_of_circuits = list(list_of_circuits)
+
     gates = [g for c in list_of_circuits for g in c._gate_list]
     capabilities = max((c._requires_capabilities for c in list_of_circuits)
                         , key=lambda x: x._bitmask)
